c3b0a220301a4b030ef1029463b6e4e9df69d423
Filip Ruzicka <f@filipruzicka.com>
2022-02-11 20:43:56 +0100

    Add preferences to init script

--- macos-init.sh
+++ macos-init.sh
@@ -19,7 +19,7 @@ if [ "$(arch)" = 'arm64' ]; then
   sudo softwareupdate --install-rosetta
 fi
 
-# Set sane defaults
+# Set sane macOS defaults
 source "$HOME/.macos"
 
 # Install Homebrew if not present
@@ -28,8 +28,33 @@ if test ! "$(which brew)"; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Install Zsh & Oh My Zsh
+# Install Oh My Zsh
 CHSH='no' RUNZSH='no' KEEP_ZSHRC='yes' sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Clean up the Dock
+brew update
+brew install dockutil
+dockutil \
+	--remove Launchpad \
+	--remove Mail \
+	--remove Maps \
+	--remove FaceTime \
+	--remove Calendar \
+	--remove Contacts \
+	--remove Reminders \
+	--remove Notes \
+	--remove TV \
+	--remove Music \
+	--remove Podcasts \
+
+# Link iTerm2 preferences file and install it
+ln -s "$HOME"/.iterm/com.googlecode.iterm2.plist "$HOME"/Library/Preferences/com.googlecode.iterm2.plist
+brew install --cask iterm2
+dockutil --add /Applications/iTerm.app
+brew cleanup
+
+# Set default shell to zsh (systems older than Catalina only)
+[ ! "$SHELL" = "/bin/zsh" ] && chsh -s /bin/zsh
 
 # Create RSA key pair
 [ ! -f "$HOME/.ssh/id_rsa.pub" ] && ssh-keygen -t rsa -f "$HOME/.ssh/id_rsa.pub" -P ""
@@ -39,5 +64,9 @@ cat "$HOME/.ssh/id_rsa.pub"
 # Install powerlevel10k custom theme
 # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Set default shell to zsh (systems older than Catalina only)
-[ ! "$SHELL" = "/bin/zsh" ] && chsh -s /bin/zsh
+printf "***\n"
+printf "Done!\n"
+printf "1. Copy the RSA public key above and put it wherever you need to put it in (GitHub, cloud servers, etc.)\n"
+printf "2. Log out/log in to apply changes\n"
+printf "3. Open iTerm2.app and continue from there (maybe install some apps using brew-mas-install.sh, too)\n"
+printf "***\n"

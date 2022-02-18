b9ca12056f81fde9b078da05c8df3475297f749f
Filip Růžička <f@filipruzicka.com>
2022-02-18 23:11:54 +0100

    Update condition in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")" || exit;
+cd "$(dirname "${BASH_SOURCE}")" || exit
 
 function syncDotFiles() {
 	rsync --exclude ".git/" \
@@ -8,18 +8,21 @@ function syncDotFiles() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE" \
-		-avh --no-perms . "$HOME";
+		-avh --no-perms . "$HOME"
 }
 
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-	syncDotFiles;
+	syncDotFiles
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		syncDotFiles;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		syncDotFiles
+	else
+		exit 0
 	fi;
 fi;
-unset syncDotFiles;
+unset syncDotFiles
 
 printf "***\nDone! next, cd to your home directory and run macos-init.sh\n***\n"
